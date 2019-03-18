51aef738ff9f26599f4ec98fe8fa02e907eb6e0d
flakessp <flakessp@gmail.com>
2019-03-18 22:32:30 +0300

    добавили шаблоницаю (сборку html с помощью JS)

--- 8/app.js
+++ 8/app.js
@@ -1,10 +1,25 @@
 // загрузить данные с адреса(в кавычках) и выполнить функцию (после запятой)
 $.get( "https://esh-lesson8.firebaseio.com/posts.json", function( data ) {
   // загруженный объект теперь доступен под именем data (имя произвольное и указывается в скобках)
-  // добавить в .post h1 свойство title из загруженного объекта
-  $('.post h1').html(data.title);
-  $('.post p').html(data.content);
 
+  // data.content 
+  // data.title
+  // data.type
+
+  let content = `<div class="post post-${data.type}">`; // превратится в post-big
+  // записываем заголовок
+  content += `<h1>${data.title}</h1>`;
+  // записывает параграф
+  content += `<p>${data.content}</p>`;
+  // закрываем тег div с классом post
+  content += '</div>';
+  
+
+  //добавить html код в элемент с классом container
+  $('.container').html(content)
+
+
+  // конец функции, загрузка закончилась
   // убрать заглушку когда данные загрузятся
-  $('.overlay').fadeOut('slow');
+  $('.overlay').animate({'border-radius': '50%', 'opacity': 0}, 2000);
 });
