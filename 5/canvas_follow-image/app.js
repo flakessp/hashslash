5b174ca0d832f80e6b53fc74c73ce0547455c448
Sergei Panfilov <flakessp@gmail.com>
2019-12-14 08:20:28 +0300

    Saving Image

--- 5/canvas_follow-image/app.js
+++ 5/canvas_follow-image/app.js
@@ -3,7 +3,7 @@ const canvasTag = document.querySelector('canvas')
 canvasTag.width = window.innerWidth
 canvasTag.height = window.innerHeight
 
-const context = canvasTag.getContext('2d')
+const ctx = canvasTag.getContext('2d')
 
 let aimX = null,
     aimY = null,
@@ -12,7 +12,7 @@ let aimX = null,
 
 let i = 0
 // get these from api? 
-const images = ['img.jpg', 'img2.jpg'].map( src => {
+const images = ['notepad.png', 'img2.jpg'].map( src => {
     const image = document.createElement('img')
     image.src = src
     return image
@@ -28,20 +28,18 @@ document.addEventListener('mousemove', event => {
     }
 })
 
-canvasTag.addEventListener('click', () => {
-    i++;
-    if (i >= images.length ){
-        i = 0;
-    }
-})
-
 const draw = () => {
     if (currentX) {
-        context.drawImage(images[i], currentX-images[i].width*0.5/2, currentY-images[i].height*0.5/2, images[i].width*0.5, images[i].height*0.5)
+        ctx.drawImage(images[i], currentX-images[i].width*0.5/2, currentY-images[i].height*0.5/2, images[i].width*0.5, images[i].height*0.5)
     }
     currentX = currentX + (aimX - currentX) * 0.1
     currentY = currentY + (aimY - currentY) * 0.1
     requestAnimationFrame(draw)
 }
-
 draw()
+
+const link = document.querySelector('.link');
+link.addEventListener('click', function(ev) {
+    link.href = canvasTag.toDataURL();
+    link.download = "mypainting.png";
+}, false);
