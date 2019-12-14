e8c4211ff03d9853345f6562a6062d1531b9ee24
Sergei Panfilov <flakessp@gmail.com>
2019-12-14 12:14:34 +0300

    вывели картинку

--- 5/canvas-start/app.js
+++ 5/canvas-start/app.js
@@ -2,8 +2,18 @@ let canvas = document.querySelector('canvas');
 
 window.addEventListener('resize', setCanvasSize);
 window.addEventListener('load', setCanvasSize);
+window.addEventListener('load', draw);
 
 function setCanvasSize(event) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+}
+
+// создание изображения 
+let img = document.createElement('img');
+img.src = 'image.png';
+let ctx = canvas.getContext('2d');
+
+function draw() {
+    ctx.drawImage(img, 100, 100, 600, 400);
 }
