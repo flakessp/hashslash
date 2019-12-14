905144af5832091e1f4a61ec5ec996a231d3b382
Sergei Panfilov <flakessp@gmail.com>
2019-12-14 14:03:01 +0300

    Смена по клику

--- 5/canvas-start/app.js
+++ 5/canvas-start/app.js
@@ -2,19 +2,31 @@ let canvas = document.querySelector('canvas');
 
 window.addEventListener('resize', setCanvasSize);
 window.addEventListener('load', setCanvasSize);
-window.addEventListener('load', draw);
 
 function setCanvasSize(event) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
-// создание изображения 
-let img = document.createElement('img');
-img.src = 'image.png';
 let ctx = canvas.getContext('2d');
+let i = 0;
 
+// массив с картинками
+let images = ['image.png', 'rose.png'].map( src => {
+    // создание изображения 
+    let img = document.createElement('img');
+    img.src = src;
+    return img;
+})
 
 document.addEventListener('mousemove', (e) => {
-    ctx.drawImage(img, e.pageX-300, e.pageY-200, 600, 400);
+    img = images[i];
+    ctx.drawImage(img, e.pageX-img.width/4, e.pageY-img.height/4, img.width/2, img.height/2);
+})
+
+canvas.addEventListener('click', () => {
+    i++;
+    if ( i == images.length ) {
+        i = 0;
+    }
 })
